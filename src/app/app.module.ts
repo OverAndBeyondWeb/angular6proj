0089233e9539929f7a17c381dd28a6f8179088f2
Juan Fox <foxj0875@gmail.com>
2018-11-26 19:44:31 -0500

    components folder structure is in place

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { RecipeBookComponent } from './recipe-book/recipe-book.component';
+import { ListComponent } from './recipe-book/list/list.component';
+import { ItemComponent } from './recipe-book/item/item.component';
+import { DetailComponent } from './recipe-book/detail/detail.component';
+import { RecipeComponent } from './recipe-book/recipe/recipe.component';
+import { ListEditComponent } from './shopping-list/list-edit/list-edit.component';
+import { IngredientComponent } from './shopping-list/ingredient/ingredient.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ShoppingListComponent,
+    RecipeBookComponent,
+    ListComponent,
+    ItemComponent,
+    DetailComponent,
+    RecipeComponent,
+    ListEditComponent,
+    IngredientComponent
   ],
   imports: [
     BrowserModule,
