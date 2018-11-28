03d5104245169a4b5f111cf25768a2462322d272
Juan Fox <foxj0875@gmail.com>
2018-11-28 09:49:51 -0500

    Event data is being sent up to RecipeBook component and back down to RecipeDetail component when a recipe from RecipeList component is clicked

--- src/app/recipe-book/detail/detail.component.ts
+++ src/app/recipe-book/detail/detail.component.ts
@@ -1,11 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-detail',
   templateUrl: './detail.component.html',
   styleUrls: ['./detail.component.scss']
 })
+
 export class DetailComponent implements OnInit {
+
+  @Input() activeRecipe: Recipe;
 
   constructor() { }
 
--- src/app/recipe-book/recipe-book.component.ts
+++ src/app/recipe-book/recipe-book.component.ts
@@ -1,12 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from './recipe.model';
 
 @Component({
   selector: 'app-recipe-book',
   templateUrl: './recipe-book.component.html',
   styleUrls: ['./recipe-book.component.scss']
 })
+
 export class RecipeBookComponent implements OnInit {
  
+  activeRecipe: Recipe;
 
   constructor() { }
 
